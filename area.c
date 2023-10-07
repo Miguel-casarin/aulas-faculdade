02fb5955a7f3680511fa08377cf010d506b12f9b
Miguel Casarin da Silva <140837596+Miguel-casarin@users.noreply.github.com>
2023-10-07 00:00:36 -0300

    Add files via upload

---
+++ area.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int raio = 2;
+    int calculo = raio * 2;
+    float pi = 3.14;
+
+    float fo = calculo * pi; 
+    printf("%f\n", fo);
+    system("pause"); 
+
+    return 0;
+}
