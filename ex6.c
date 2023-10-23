a4199b57609def037b5e71276634c7981fdf07e7
Miguel Casarin da Silva <140837596+Miguel-casarin@users.noreply.github.com>
2023-10-23 14:48:17 -0300

    Add files via upload

---
+++ ex6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int a = 0; //acumulador
+	int c =1;  // soma
+	
+	while( a <= 100){
+		
+		a +=1;
+		c++;
+		
+	}
+	printf("%i", c);
+	
+return 0;
+}
