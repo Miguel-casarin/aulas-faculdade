c673634fcf2e4799f44e281f07813edf9da6482a
Miguel Casarin da Silva <140837596+Miguel-casarin@users.noreply.github.com>
2023-11-20 15:45:05 -0300

    Add files via upload

---
+++ compara.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int v[5] = {4,1,5,8,6};
+int n = 5;
+int menor = v[0];
+int maior = v[0];
+
+for (int i = 0; i < n; i++) {
+int t = v[i];
+if (t > maior) {
+maior = t;
+}
+if (t < menor) {
+menor = t;
+}
+}
+
+printf("menor valor: %i\n", menor);
+printf("maior valor: %i\n", maior);
+
+return 0;
+}
+
+
